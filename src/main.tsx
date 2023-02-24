112daf04ffd2040ae1dba1dac41ea52b9fbf8539
caonam <nam52871@donga.edu.vn>
2023-02-24 19:47:26 +0700

    Config Axios and React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,26 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false // disable retrieving data every time the window is displayed.
+    }
+  }
+})
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 )
---
+++ src/utils/http.ts
@@ -0,0 +1,16 @@
+import axios, { type AxiosInstance } from 'axios'
+
+class Http {
+  instance: AxiosInstance
+  constructor() {
+    this.instance = axios.create({
+      baseURL: 'https://api-ecom.duthanhduoc.com/',
+      timeout: 10000,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  }
+}
+
+export const http = new Http().instance
