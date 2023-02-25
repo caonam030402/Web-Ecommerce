0dc70809147636a70733caff781c3c503a99cd30
caonam <nam52871@donga.edu.vn>
2023-02-25 12:54:29 +0700

    Simple message processing with Axios Interceptor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import useRouteElements from './useRouteElements'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const routeElements = useRouteElements()
-  return <div>{routeElements}</div>
+  return (
+    <div>
+      {routeElements}
+      <ToastContainer />
+    </div>
+  )
 }
 export default App
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,6 @@
+import { HttpStatusCode } from './../constants/httpStatusCode'
 import axios, { type AxiosInstance } from 'axios'
+import { toast } from 'react-toastify'
 
 class Http {
   instance: AxiosInstance
@@ -10,6 +12,21 @@ class Http {
         'Content-Type': 'application/json'
       }
     })
+
+    //Error handling in addition to error 422
+    this.instance.interceptors.response.use(
+      function (response) {
+        return response
+      },
+      function (error) {
+        if (error.response.status !== HttpStatusCode.UnprocessableEntity) {
+          const data: any | undefined = error.response?.data
+          const message = data.message || error.message
+          toast.error(message)
+        }
+        return Promise.reject(error)
+      }
+    )
   }
 }
 
