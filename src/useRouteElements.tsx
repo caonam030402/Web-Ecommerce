e285aa9b27f2d5770d174caf88ff907c86653433
caonam <nam52871@donga.edu.vn>
2023-02-27 10:55:02 +0700

    Setup Protected Route and Rejected Route with React Router

--- src/useRouteElements.tsx
+++ src/useRouteElements.tsx
@@ -1,13 +1,25 @@
-import { useRoutes } from 'react-router-dom'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import RegisterLayout from './layouts/RegisterLayout'
 import Login from './pages/Login'
 import ProductList from './pages/ProductList'
+import Profile from './pages/Profile'
 import Register from './pages/Register'
+
+const isAuthenticated = false
+function ProtectedRoute() {
+  return isAuthenticated ? <Outlet /> : <Navigate to='/login' />
+}
+
+function RejectedRoute() {
+  return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
+}
 
 export default function useRouteElements() {
   const routeElements = useRoutes([
+    // Home Page
     {
+      index: true,
       path: '/',
       element: (
         <MainLayout>
@@ -15,21 +27,45 @@ export default function useRouteElements() {
         </MainLayout>
       )
     },
+
+    // Profile Page
     {
-      path: '/register',
-      element: (
-        <RegisterLayout>
-          <Register />
-        </RegisterLayout>
-      )
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: '/profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          )
+        }
+      ]
     },
+
+    // Login - Register Page
     {
-      path: '/login',
-      element: (
-        <RegisterLayout>
-          <Login />
-        </RegisterLayout>
-      )
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: '/register',
+          element: (
+            <RegisterLayout>
+              <Register />
+            </RegisterLayout>
+          )
+        },
+        {
+          path: '/login',
+          element: (
+            <RegisterLayout>
+              <Login />
+            </RegisterLayout>
+          )
+        }
+      ]
     }
   ])
   return routeElements
