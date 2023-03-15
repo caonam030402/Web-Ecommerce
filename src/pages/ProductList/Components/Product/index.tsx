e89b62766be2ff0d792db8a01bc31f79035b5f28
caonam <nam52871@donga.edu.vn>
2023-03-15 22:04:33 +0700

    Display product list and format number with Intl

--- src/pages/ProductList/Components/Product/index.tsx
+++ src/pages/ProductList/Components/Product/index.tsx
@@ -1,24 +1,28 @@
 import { Link } from 'react-router-dom'
 import { BsStarFill, BsStar } from 'react-icons/bs'
+import { Product as ProductType } from 'src/types/product.type'
+import { formatCurrency, formatNumberToSocialStyle } from 'src/utils/utils'
 
-export default function ProductItem() {
+interface Props {
+  product: ProductType
+}
+
+export default function ProductItem({ product }: Props) {
   return (
     <Link to=''>
       <div className='rounded-sm bg-white shadow-sm transition-none duration-100 hover:translate-y-[-1px]'>
         <div className='relative w-full pt-[100%]'>
           <img
-            className='absolute top-0 right-0 rounded-t-sm object-cover'
-            src='https://cf.shopee.vn/file/5ad0b8b727974ec66dc47baa9c329047_tn'
+            className='absolute top-0 right-0 h-full w-[100%] rounded-t-sm object-cover'
+            src={product.image}
             alt=''
           />
         </div>
         <div className=' p-2 '>
-          <p className='min-h-[2rem] text-xs line-clamp-2'>
-            Tai nghe bluetooth TWS 5.0 Không Dây Chuẩn HIFI Bản Cao Cấp
-          </p>
+          <p className='min-h-[2rem] text-xs line-clamp-2'>{product.name}</p>
           <div className='mt-1'>
-            <span className='line-through'>₫200.000</span>
-            <span className='ml-[5px] text-base text-primaryColor'>₫100.000</span>
+            <span className='line-through'>{product.price_before_discount}</span>
+            <span className='ml-[5px] text-base text-primaryColor'>{product.price}</span>
           </div>
           <div className='mt-5 flex items-center'>
             <div className='flex items-center text-[9px]'>
@@ -28,7 +32,7 @@ export default function ProductItem() {
               <BsStarFill className='mr-[5px] text-yellow-500' />
               <BsStarFill className='mr-[5px] text-yellow-500' />
             </div>
-            <div className='text-xs'>Đã bán 4,6k</div>
+            <div className='text-xs'>Đã bán {formatNumberToSocialStyle(product.sold)} </div>
           </div>
         </div>
       </div>
--- src/pages/ProductList/index.tsx
+++ src/pages/ProductList/index.tsx
@@ -28,11 +28,10 @@ export default function ProductList() {
           <div className='col-span-10'>
             <SortProductList />
             <div className='mt-6 grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-              {Array(30)
-                .fill(0)
-                .map((_, index) => (
-                  <div className='' key={index}>
-                    <ProductItem />
+              {data &&
+                data?.data.data.products.map((product) => (
+                  <div className='' key={product._id}>
+                    <ProductItem product={product} />
                   </div>
                 ))}
             </div>
