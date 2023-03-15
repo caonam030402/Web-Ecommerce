1e1ad854f1d63c9f4d51eec9d81f2876516ca377
caonam <nam52871@donga.edu.vn>
2023-03-15 23:14:44 +0700

    Processing Product Rating

--- src/pages/ProductList/Components/Product/index.tsx
+++ src/pages/ProductList/Components/Product/index.tsx
@@ -1,7 +1,8 @@
 import { Link } from 'react-router-dom'
-import { BsStarFill, BsStar } from 'react-icons/bs'
+
 import { Product as ProductType } from 'src/types/product.type'
 import { formatCurrency, formatNumberToSocialStyle } from 'src/utils/utils'
+import ProductRating from 'src/components/ProductRating'
 
 interface Props {
   product: ProductType
@@ -21,18 +22,12 @@ export default function ProductItem({ product }: Props) {
         <div className=' p-2 '>
           <p className='min-h-[2rem] text-xs line-clamp-2'>{product.name}</p>
           <div className='mt-1'>
-            <span className='line-through'>{product.price_before_discount}</span>
-            <span className='ml-[5px] text-base text-primaryColor'>{product.price}</span>
+            <span className='line-through'>{formatCurrency(product.price_before_discount)}</span>
+            <span className='ml-[5px] text-base text-primaryColor'>{formatCurrency(product.price)}</span>
           </div>
           <div className='mt-5 flex items-center'>
-            <div className='flex items-center text-[9px]'>
-              <BsStarFill className='mr-[5px] text-yellow-500' />
-              <BsStarFill className='mr-[5px] text-yellow-500' />
-              <BsStarFill className='mr-[5px] text-yellow-500' />
-              <BsStarFill className='mr-[5px] text-yellow-500' />
-              <BsStarFill className='mr-[5px] text-yellow-500' />
-            </div>
-            <div className='text-xs'>Đã bán {formatNumberToSocialStyle(product.sold)} </div>
+            <ProductRating rating={product.rating} />
+            <div className='ml-2 text-xs'>Đã bán {formatNumberToSocialStyle(product.sold)} </div>
           </div>
         </div>
       </div>
