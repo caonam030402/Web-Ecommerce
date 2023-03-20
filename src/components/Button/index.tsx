9922ef6ef5ba42e83888b9fae55bf4c0b218c1a3
caonam <nam52871@donga.edu.vn>
2023-03-20 20:29:42 +0700

    Handle empty productList

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,7 +5,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export default function Button(props: ButtonProps) {
-  const { className, isLoading, disabled, children, ...rest } = props
+  const { className, disabled, children, ...rest } = props
   const newClassName = disabled ? className + ' cursor-not-allowed opacity-70' : className
 
   return (
--- src/pages/ProductList/index.tsx
+++ src/pages/ProductList/index.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
 import { omitBy, isUndefined } from 'lodash'
+import { AiOutlineFileSearch } from 'react-icons/ai'
 import { categoryApi } from 'src/apis/category.api'
 import { productApi } from 'src/apis/product.api'
+import Button from 'src/components/Button'
 import Pagination from 'src/components/Pagination'
 import Slide from 'src/components/Slide'
 import UseQueryParams from 'src/hooks/UseQueryParams'
@@ -48,6 +50,13 @@ export default function ProductList() {
     }
   })
 
+  const checkProductListEmpty = () => {
+    if (productData) {
+      return productData.data.data.products.length === 0
+    }
+  }
+
+  console.log(checkProductListEmpty())
   return (
     <div>
       <div>
@@ -59,17 +68,30 @@ export default function ProductList() {
             <div className='col-span-2'>
               <AsideFitter queryConfig={queryConfig} categories={categoriesData?.data.data || []} />
             </div>
-            <div className='col-span-10'>
-              <SortProductList queryConfig={queryConfig} pageSize={productData.data.data.pagination.page_size} />
-              <div className='mt-6 grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
-                {productData?.data.data.products.map((product) => (
-                  <div className='' key={product._id}>
-                    <ProductItem product={product} />
-                  </div>
-                ))}
+            {!checkProductListEmpty() && (
+              <div className='col-span-10'>
+                <SortProductList queryConfig={queryConfig} pageSize={productData.data.data.pagination.page_size} />
+                <div className='mt-6 grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5'>
+                  {productData?.data.data.products.map((product) => (
+                    <div className='' key={product._id}>
+                      <ProductItem product={product} />
+                    </div>
+                  ))}
+                </div>
+                <Pagination queryConfig={queryConfig} pageSize={productData.data.data.pagination.page_size} />
               </div>
-              <Pagination queryConfig={queryConfig} pageSize={productData.data.data.pagination.page_size} />
-            </div>
+            )}
+
+            {checkProductListEmpty() && (
+              <div className='col-span-10 flex flex-col items-center justify-center text-gray-500'>
+                <AiOutlineFileSearch className='text-[100px]' />
+                <p className='mt-3 text-lg'>Hix. Không có sản phẩm nào. Bạn thử tắt điều kiện lọc và tìm lại nhé?</p>
+                <p className='mt-4'>Or</p>
+                <Button className='mt-4 flex w-[13%] items-center justify-center rounded-sm bg-primaryColor p-2 text-base text-white hover:opacity-90'>
+                  Xóa Bộ Lọc
+                </Button>
+              </div>
+            )}
           </div>
         )}
       </div>
